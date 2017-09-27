cf457c82ff581e6d77b89075f7a89917364b0091
azeem59 <azeem59@gmail.com>
2017-09-27 09:37:20 +0200

    Initial commit.

--- demo_regression.sh
+++ demo_regression.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp arrest_demo.jar:jdom-2.0.6.jar:commons-lang3-3.4.jar:jsoup-1.9.2.jar demo.DemoRegression /Users/azeemahmad/Downloads/Demo_Francisco/testsuites_trials1/regression/
+java -cp arrest_demo.jar:jdom-2.0.6.jar:commons-lang3-3.4.jar:jsoup-1.9.2.jar demo.DemoRegression /Users/azeemahmad/Downloads/Demo_Francisco/testsuites/regression/
--- selection_demo.sh
+++ selection_demo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp demo_technique.jar:jdom-2.0.6.jar:commons-lang3-3.4.jar:jsoup-1.9.2.jar demo.DemoSelection 0.2 /Users/azeemahmad/Downloads/Demo_Francisco/testsuites_trials1/selection/
+java -cp demo_technique.jar:jdom-2.0.6.jar:commons-lang3-3.4.jar:jsoup-1.9.2.jar demo.DemoSelection 0.2 /Users/azeemahmad/Downloads/Demo_Francisco/testsuites/selection/
